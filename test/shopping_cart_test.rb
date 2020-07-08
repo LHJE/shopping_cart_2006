d9009d448323f16c47d69dac4d0384b9fb2f4822
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-07-08 09:35:29 -0600

    Variable testing working

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -10,4 +10,16 @@ class ShoppingCartTest < Minitest::Test
     assert_instance_of ShoppingCart, cart
   end
 
+  def test_it_has_a_store_name
+    cart = ShoppingCart.new("King Soopers", "30items")
+
+    assert_equal "King Soopers", cart.store_name
+  end
+
+
+  def test_it_has_an_item_amount
+    cart = ShoppingCart.new("King Soopers", "30items")
+
+    assert_equal "30items", cart.item_amount
+  end
 end
