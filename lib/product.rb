5af3c93f40c359656dbdc6b9d3104b2a7812295b
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-07-08 09:09:01 -0600

    Make Product class

---
+++ lib/product.rb
@@ -0,0 +1,18 @@
+class Product
+
+  def initialize
+    @category = category
+    @name = name
+    @unit_price = unit_price
+    @quantity = quantity
+  end
+
+  def total_price
+    
+  end
+
+
+
+
+
+end
