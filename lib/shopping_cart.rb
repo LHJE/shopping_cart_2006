2a909cf93894f5e16de893b0e3e79c3176358ea8
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-07-08 09:33:35 -0600

    First ShoppingCart test running

---
+++ lib/shopping_cart.rb
@@ -0,0 +1,33 @@
+require './lib/product'
+
+class ShoppingCart
+
+  attr_reader :store_name, :item_amount
+  def initialize(store_name, item_amount)
+    @store_name = store_name
+    @item_amount = item_amount
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
